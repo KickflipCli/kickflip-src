496adc0ad51951e31e9116b157ef6ce016f16066
Dan Pock <self@danpock.me>
2021-12-07 18:47:50 -0500

    make sure mono repo can highlight correctly

---
+++ _ide_helper.php
@@ -0,0 +1,42 @@
+<?php
+// @formatter:off
+
+/**
+ * A helper file for Laravel, to provide autocomplete information to your IDE
+ * Generated for Laravel unreleased.
+ *
+ * This file should not be included in your code, only analyzed by your IDE!
+ *
+ * @author Barry vd. Heuvel <barryvdh@gmail.com>
+ * @see https://github.com/barryvdh/laravel-ide-helper
+ */
+    namespace Illuminate\Support {
+        class Stringable {
+        /**
+         *
+         *
+         * @see \Kickflip\KickflipKernel::__construct()
+         * @param string $env
+         * @static
+         */
+        public static function replaceEnv($env)
+        {
+            return \Illuminate\Support\Stringable::replaceEnv($env);
+        }
+        /**
+         *
+         *
+         * @see \Kickflip\KickflipKernel::__construct()
+         * @static
+         */
+        public static function findVerbosity()
+        {
+            return \Illuminate\Support\Stringable::findVerbosity();
+        }
+
+    }
+
+}
+
+
+
