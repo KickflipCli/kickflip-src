7f5adb3da33e49c821248de27a40aa7e4fd3d251
Dan Pock <self@danpock.me>
2021-12-06 21:14:39 -0500

    adjust packages for monorepo

--- monorepo-builder.php
+++ monorepo-builder.php
@@ -3,8 +3,6 @@
 declare(strict_types=1);
 
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
-use Symplify\ComposerJsonManipulator\ValueObject\ComposerJsonSection;
-use Symplify\MonorepoBuilder\ValueObject\Option;
 use Symplify\MonorepoBuilder\Release\ReleaseWorker\AddTagToChangelogReleaseWorker;
 use Symplify\MonorepoBuilder\Release\ReleaseWorker\PushNextDevReleaseWorker;
 use Symplify\MonorepoBuilder\Release\ReleaseWorker\PushTagReleaseWorker;
@@ -13,16 +11,11 @@ use Symplify\MonorepoBuilder\Release\ReleaseWorker\SetNextMutualDependenciesRele
 use Symplify\MonorepoBuilder\Release\ReleaseWorker\TagVersionReleaseWorker;
 use Symplify\MonorepoBuilder\Release\ReleaseWorker\UpdateBranchAliasReleaseWorker;
 use Symplify\MonorepoBuilder\Release\ReleaseWorker\UpdateReplaceReleaseWorker;
-
+use Symplify\MonorepoBuilder\ValueObject\Option;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
     $parameters = $containerConfigurator->parameters();
-    // for "merge" command
-    $parameters->set(Option::DATA_TO_APPEND, [
-        ComposerJsonSection::REQUIRE_DEV => [
-            'phpunit/phpunit' => '^9.5',
-        ],
-    ]);
+    $parameters->set(Option::DEFAULT_BRANCH_NAME, 'main');
 
     $services = $containerConfigurator->services();
     # release workers - in order to execute
--- packages/kickflip-cli/tests/Unit/KickflipHelperTest.php
+++ packages/kickflip-cli/tests/Unit/KickflipHelperTest.php
@@ -12,11 +12,12 @@ test('default KickflipHelper::basePath', function () {
 test('custom KickflipHelper::basePath', function ($input, $expected) {
     expect(KickflipHelper::basePath($input))
         ->toBeString()
-        ->toBe(dirname(__DIR__, 2) . $expected);
+        ->toBe(dirname(__DIR__, 4) . $expected);
 })->with([
-    [null, '/tests/mock-app'],
-    ['./', ''],
-    ['./packages/kickflip-docs', '/packages/kickflip-docs'],
+    [null, '/packages/kickflip-cli/tests/mock-app'],
+    ['./', '/packages/kickflip-cli'],
+    ['../../', ''],
+    ['../kickflip-docs', '/packages/kickflip-docs'],
 ]);
 
 test('helper to kebab', function ($input, $expected) {
