1dee7fe5aa018affc117b57792b68fab3327e2f5
Dan Pock <self@danpock.me>
2021-12-09 13:36:12 -0500

    remove all but a few workers
    RESULT
    ± |main → public {1} ✓| → ./vendor/bin/monorepo-builder release minor --dry-run
    
    1/2) Update "replace" version in "composer.json" to new tag to avoid circular dependencies conflicts
    ====================================================================================================
    
    2/2) Set packages mutual dependencies to "^0.2" version
    =======================================================
    
     ! [NOTE] Running in dry mode, nothing is changed

--- monorepo-builder.php
+++ monorepo-builder.php
@@ -25,10 +25,10 @@ return static function (ContainerConfigurator $containerConfigurator): void {
     $services->set(UpdateReplaceReleaseWorker::class);
     $services->set(SetCurrentMutualDependenciesReleaseWorker::class);
     $services->set(ConfigEnvironmentReplacerReleaseWorker::class);
-    $services->set(AddTagToChangelogReleaseWorker::class);
-    $services->set(TagVersionReleaseWorker::class);
-    $services->set(PushTagReleaseWorker::class);
-    $services->set(SetNextMutualDependenciesReleaseWorker::class);
-    $services->set(UpdateBranchAliasReleaseWorker::class);
-    $services->set(PushNextDevReleaseWorker::class);
+    //$services->set(AddTagToChangelogReleaseWorker::class);
+    //$services->set(TagVersionReleaseWorker::class);
+    //$services->set(PushTagReleaseWorker::class);
+    //$services->set(SetNextMutualDependenciesReleaseWorker::class);
+    //$services->set(UpdateBranchAliasReleaseWorker::class);
+    //$services->set(PushNextDevReleaseWorker::class);
 };
