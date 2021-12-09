c5d61262768b0208739a6cf4b2b1a62aba49bdc6
Dan Pock <self@danpock.me>
2021-12-09 14:06:26 -0500

    back to normal

--- monorepo-builder.php
+++ monorepo-builder.php
@@ -3,6 +3,7 @@
 declare(strict_types=1);
 
 use Nette\Utils\Strings;
+use RepoBuilder\ConfigEnvironmentReplacerReleaseWorker;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symplify\MonorepoBuilder\Release\Contract\ReleaseWorker\ReleaseWorkerInterface;
 use Symplify\MonorepoBuilder\Release\ReleaseWorker\AddTagToChangelogReleaseWorker;
@@ -15,28 +16,6 @@ use Symplify\MonorepoBuilder\Release\ReleaseWorker\UpdateBranchAliasReleaseWorke
 use Symplify\MonorepoBuilder\Release\ReleaseWorker\UpdateReplaceReleaseWorker;
 use Symplify\MonorepoBuilder\ValueObject\Option;
 
-if (!class_exists('ConfigEnvironmentReplacerReleaseWorker')) {
-    final class ConfigEnvironmentReplacerReleaseWorker implements ReleaseWorkerInterface
-    {
-        public function work($version): void
-        {
-            $configFile = \getcwd() . '/config/app.php';
-            if (!\file_exists($configFile)) {
-                return;
-            }
-            $configFileContent = \file_get_contents($configFile);
-            $configFileContent = Strings::replace($configFileContent, "/'env' => '([a-z]+)'/", "'env' => 'production'");
-            \file_put_contents($configFile, $configFileContent);
-        }
-
-        public function getDescription($version): string
-        {
-            return 'Update config/app.php to production environment';
-        }
-    }
-
-}
-
 return static function (ContainerConfigurator $containerConfigurator): void {
     $parameters = $containerConfigurator->parameters();
     $parameters->set(Option::DEFAULT_BRANCH_NAME, 'main');
@@ -46,7 +25,7 @@ return static function (ContainerConfigurator $containerConfigurator): void {
     # release workers - in order to execute
     $services->set(UpdateReplaceReleaseWorker::class);
     $services->set(SetCurrentMutualDependenciesReleaseWorker::class);
-    $services->set(\RepoBuilder\ConfigEnvironmentReplacerReleaseWorker::class, \RepoBuilder\ConfigEnvironmentReplacerReleaseWorker::class);
+    $services->set(ConfigEnvironmentReplacerReleaseWorker::class);
     //$services->set(AddTagToChangelogReleaseWorker::class);
     //$services->set(TagVersionReleaseWorker::class);
     //$services->set(PushTagReleaseWorker::class);
