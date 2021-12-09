1f58e2974f45f5745b1e08dbef5bb42fccc69480
Dan Pock <self@danpock.me>
2021-12-09 13:59:53 -0500

    try the namespaced class instead now...

--- monorepo-builder.php
+++ monorepo-builder.php
@@ -46,7 +46,7 @@ return static function (ContainerConfigurator $containerConfigurator): void {
     # release workers - in order to execute
     $services->set(UpdateReplaceReleaseWorker::class);
     $services->set(SetCurrentMutualDependenciesReleaseWorker::class);
-    $services->set(ConfigEnvironmentReplacerReleaseWorker::class, ConfigEnvironmentReplacerReleaseWorker::class);
+    $services->set(\RepoBuilder\ConfigEnvironmentReplacerReleaseWorker::class, \RepoBuilder\ConfigEnvironmentReplacerReleaseWorker::class);
     //$services->set(AddTagToChangelogReleaseWorker::class);
     //$services->set(TagVersionReleaseWorker::class);
     //$services->set(PushTagReleaseWorker::class);
