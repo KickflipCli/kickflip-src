4fafd983aab8387b665c5848b65efb83bf0528ae
Dan Pock <self@danpock.me>
2021-12-09 18:16:34 -0500

    add changelog and clean up some files

--- monorepo-builder.php
+++ monorepo-builder.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 use Nette\Utils\Strings;
 use RepoBuilder\ConfigEnvironmentReplacerReleaseWorker;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
-use Symplify\MonorepoBuilder\Release\Contract\ReleaseWorker\ReleaseWorkerInterface;
 use Symplify\MonorepoBuilder\Release\ReleaseWorker\AddTagToChangelogReleaseWorker;
 use Symplify\MonorepoBuilder\Release\ReleaseWorker\PushNextDevReleaseWorker;
 use Symplify\MonorepoBuilder\Release\ReleaseWorker\PushTagReleaseWorker;
@@ -25,7 +24,10 @@ return static function (ContainerConfigurator $containerConfigurator): void {
     # release workers - in order to execute
     $services->set(UpdateReplaceReleaseWorker::class);
     $services->set(SetCurrentMutualDependenciesReleaseWorker::class);
+
+    // Update kickflip config to production mode
     $services->set(ConfigEnvironmentReplacerReleaseWorker::class);
+
     $services->set(AddTagToChangelogReleaseWorker::class);
     $services->set(TagVersionReleaseWorker::class);
     $services->set(PushTagReleaseWorker::class);
