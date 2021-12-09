be7e3dae132fb2a1a98f044dd435a972519e29ef
Dan Pock <self@danpock.me>
2021-12-09 13:55:15 -0500

    try in-file class definition

--- monorepo-builder.php
+++ monorepo-builder.php
@@ -2,7 +2,9 @@
 
 declare(strict_types=1);
 
+use Nette\Utils\Strings;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symplify\MonorepoBuilder\Release\Contract\ReleaseWorker\ReleaseWorkerInterface;
 use Symplify\MonorepoBuilder\Release\ReleaseWorker\AddTagToChangelogReleaseWorker;
 use Symplify\MonorepoBuilder\Release\ReleaseWorker\PushNextDevReleaseWorker;
 use Symplify\MonorepoBuilder\Release\ReleaseWorker\PushTagReleaseWorker;
@@ -13,7 +15,27 @@ use Symplify\MonorepoBuilder\Release\ReleaseWorker\UpdateBranchAliasReleaseWorke
 use Symplify\MonorepoBuilder\Release\ReleaseWorker\UpdateReplaceReleaseWorker;
 use Symplify\MonorepoBuilder\ValueObject\Option;
 
-use RepoBuilder\ConfigEnvironmentReplacerReleaseWorker;
+if (!class_exists('ConfigEnvironmentReplacerReleaseWorker')) {
+    final class ConfigEnvironmentReplacerReleaseWorker implements ReleaseWorkerInterface
+    {
+        public function work($version): void
+        {
+            $configFile = \getcwd() . '/config/app.php';
+            if (!\file_exists($configFile)) {
+                return;
+            }
+            $configFileContent = \file_get_contents($configFile);
+            $configFileContent = Strings::replace($configFileContent, "/'env' => '([a-z]+)'/", "'env' => 'production'");
+            \file_put_contents($configFile, $configFileContent);
+        }
+
+        public function getDescription($version): string
+        {
+            return 'Update config/app.php to production environment';
+        }
+    }
+
+}
 
 return static function (ContainerConfigurator $containerConfigurator): void {
     $parameters = $containerConfigurator->parameters();
