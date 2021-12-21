b2f17750394ebb045613047bdab9453e0d524333
Dan Pock <self@danpock.me>
2021-12-21 16:58:46 -0500

    switch navigation to use mix of route/url methods

--- packages/kickflip-docs/config/navigation.php
+++ packages/kickflip-docs/config/navigation.php
@@ -3,28 +3,28 @@
 use Kickflip\Models\NavItem;
 
 return [
-    NavItem::make('Getting Started', getDocUrl('getting-started'))
+    NavItem::make('Getting Started', route('docs.getting-started'))
         ->setChildren([
-            NavItem::make('Installation', getDocUrl('getting-started-installation')),
-            NavItem::make('Directory Structure', getDocUrl('getting-started-structure')),
+            NavItem::make('Installation', url('#docs.getting-started-installation')),
+            NavItem::make('Directory Structure', route('docs.getting-started-structure')),
         ]),
-    NavItem::make('Building & Previewing', getDocUrl('building-and-previewing'))
+    NavItem::make('Building & Previewing', route('docs.building-and-previewing'))
         ->setChildren([
-            NavItem::make('Environments', getDocUrl('building-and-previewing-environments')),
-            NavItem::make('Compiling Assets', getDocUrl('building-and-previewing-compiling-assets')),
+            NavItem::make('Environments', route('docs.building-and-previewing-environments')),
+            NavItem::make('Compiling Assets', route('docs.building-and-previewing-compiling-assets')),
         ]),
-    NavItem::make('Creating your Site\'s Content', getDocUrl('site-content'))
+    NavItem::make('Creating your Site\'s Content', route('docs.site-content'))
         ->setChildren([
-            NavItem::make('Templates & Components', getDocUrl('site-content-templates-and-components')),
-            NavItem::make('Markdown', getDocUrl('site-content-markdown')),
-            NavItem::make('Other File Types', getDocUrl('site-content-other-file-types')),
+            NavItem::make('Templates & Components', route('docs.site-content-templates-and-components')),
+            NavItem::make('Markdown', url('#docs.site-content-markdown')),
+            NavItem::make('Other File Types', url('#docs.site-content-other-file-types')),
         ]),
-    NavItem::make('Template Variables', getDocUrl('template-variables'))
+    NavItem::make('Template Variables', route('docs.template-variables'))
         ->setChildren([
-            NavItem::make('Site Variables', getDocUrl('template-variables-site')),
-            NavItem::make('Page Data', getDocUrl('template-variables-page')),
+            NavItem::make('Site Variables', route('docs.template-variables-site')),
+            NavItem::make('Page Data', route('docs.template-variables-page')),
         ]),
-    NavItem::make('Pretty URLs', getDocUrl('pretty-urls')),
-    NavItem::make('Event Listeners', getDocUrl('event-listeners')),
-    NavItem::make('Navigation', getDocUrl('navigation')),
+    NavItem::make('Pretty URLs', url('#docs.pretty-urls')),
+    NavItem::make('Event Listeners', route('docs.event-listeners')),
+    NavItem::make('Navigation', route('docs.navigation')),
 ];
