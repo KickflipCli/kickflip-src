e28d191bf0b81607f324072d91252a5708e42e35
MallardDuck <self@danpock.me>
2021-12-27 12:12:38 -0500

    ensure failure test cleans up after itslef

--- tests/Feature/ShikiFetcher/ShikiFetcherFailureTest.php
+++ tests/Feature/ShikiFetcher/ShikiFetcherFailureTest.php
@@ -25,10 +25,18 @@ afterEach(function () {
 });
 
 it('will throw an exception if shiki fetcher fails', function () {
+    // Create shiki fetcher
     $shikiFetcher = new ShikiNpmFetcher();
     $nodeModules = $shikiFetcher->getProjectRootDirectory() . '/node_modules';
+    // Setup directory that will cause failure...
     mkdir($nodeModules, '0500');
     chmod($nodeModules, 0500);
+    expect($nodeModules)->toBeDirectory();
+    // Expect the exception and trigger the failure
     $this->expectException(\Symfony\Component\Process\Exception\ProcessFailedException::class);
     $shikiFetcher->installShiki();
+    // Ensure 0500 perms directory is removed
+    chmod($nodeModules, 0700);
+    rmdir($nodeModules);
+    expect($nodeModules)->not->toBeDirectory();
 });
