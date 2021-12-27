1d834b514f7b8127e4069a26a2b4b4b25948dfb0
MallardDuck <self@danpock.me>
2021-12-26 19:39:58 -0500

    update logging stack driver

--- packages/kickflip-cli/config/logging.php
+++ packages/kickflip-cli/config/logging.php
@@ -37,7 +37,7 @@ return [
     'channels' => [
         'stack' => [
             'driver' => 'stack',
-            'channels' => ['stderr'],
+            'channels' => ['single', 'stderr'],
             'ignore_exceptions' => false,
         ],
 
