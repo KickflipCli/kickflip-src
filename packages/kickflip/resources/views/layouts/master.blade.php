b2e8fd26ff6bf096350c063de07971cf95883ae0
MallardDuck <self@danpock.me>
2021-12-27 19:07:30 -0500

    correct dark mode colors

--- packages/kickflip/resources/views/layouts/master.blade.php
+++ packages/kickflip/resources/views/layouts/master.blade.php
@@ -29,7 +29,7 @@
     </head>
 
     <body class="flex flex-col justify-between min-h-screen bg-gray-100 text-gray-800 leading-normal font-sans">
-        <header class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0" role="banner">
+        <header class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:text-neutral-50 dark:bg-gray-900 sm:items-center py-4 sm:pt-0" role="banner">
             Hello, world!
             <x-emoji-icon::skateboard class="text-5xl" />
         </header>
