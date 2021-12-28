4ffdb15bb3ec617f363d62163950c48252092a4e
MallardDuck <self@danpock.me>
2021-12-27 20:32:44 -0500

    add code of conduct and update footer

--- packages/kickflip-docs/resources/views/layouts/footer.blade.php
+++ packages/kickflip-docs/resources/views/layouts/footer.blade.php
@@ -1,5 +1,5 @@
 <footer class="bg-white text-center text-sm py-4 border-t" role="contentinfo">
-    <ul class="flex flex-col md:flex-row justify-center">
+    <ul class="flex flex-col md:flex-row justify-center mb-1">
         <li class="md:mr-2">
             Built by <a href="https://github.com/mallardduck">MallardDuck</a> with Kickflip!
         </li>
@@ -8,6 +8,14 @@
             <a href="https://github.com/mallardduck/kickflip" title="Kickflip on GitHub" target="_blank">
                 <span class="hidden md:inline-block hover:underline">Contribute on GitHub</span>
                 <x-bxl-github class="md:hidden h-8 md:h-10" />
+            </a>
+        </li>
+    </ul>
+    <ul class="flex flex-col md:flex-row justify-center mt-1">
+        <li>
+            <span>Proudly hosted by </span>
+            <a href="https://www.netlify.com/pricing/" title="Netlify Free Hosting" target="_blank">
+                <span class="hover:underline">Netlify</span>
             </a>
         </li>
     </ul>
