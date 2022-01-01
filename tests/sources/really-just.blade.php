6585985f2983ef915c2ede7c659a2be20ac54b34
MallardDuck <self@danpock.me>
2022-01-01 17:50:15 -0500

    fix blade example stub

--- tests/sources/really-just.blade.php
+++ tests/sources/really-just.blade.php
@@ -1,9 +1,11 @@
----
-date: 2018-02-16
----
+@php
+$date = "2018-02-16";
+@endphp
+
 @extends('layouts.post')
 
 @section('postContent')
     <h1>Post Stuff</h1>
     <p>Post content stuff.</p>
+    {{ $date }}
 @endsection
