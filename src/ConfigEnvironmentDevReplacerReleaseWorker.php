fe603a008bae520fd4ee172f9390d77b8ca597bc
MallardDuck <self@danpock.me>
2022-01-02 21:25:13 -0500

    imporve monorepo scripts

--- src/ConfigEnvironmentDevReplacerReleaseWorker.php
+++ src/ConfigEnvironmentDevReplacerReleaseWorker.php
@@ -1,24 +1,32 @@
 <?php
 
+declare(strict_types=1);
+
 namespace RepoBuilder;
 
 use Nette\Utils\Strings;
+use PharIo\Version\Version;
 use Symplify\MonorepoBuilder\Release\Contract\ReleaseWorker\ReleaseWorkerInterface;
+
+use function file_exists;
+use function file_get_contents;
+use function file_put_contents;
+use function getcwd;
 
 final class ConfigEnvironmentDevReplacerReleaseWorker implements ReleaseWorkerInterface
 {
-    public function work($version): void
+    public function work(Version $version): void
     {
-        $configFile = \getcwd() . '/packages/kickflip-cli/config/app.php';
-        if (!\file_exists($configFile)) {
+        $configFile = getcwd() . '/packages/kickflip-cli/config/app.php';
+        if (!file_exists($configFile)) {
             return;
         }
-        $configFileContent = \file_get_contents($configFile);
+        $configFileContent = file_get_contents($configFile);
         $configFileContent = Strings::replace($configFileContent, "/'env' => '([a-z]+)'/", "'env' => 'development'");
-        \file_put_contents($configFile, $configFileContent);
+        file_put_contents($configFile, $configFileContent);
     }
 
-    public function getDescription($version): string
+    public function getDescription(Version $version): string
     {
         return 'Revert config/app.php to development environment';
     }
