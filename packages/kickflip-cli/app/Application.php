72f8c007f13e5c683e9b17595088b3a9e512f60d
Dan Pock <dpock@liquidweb.com>
2022-01-04 23:42:00 -0500

    fix(core-cli): correct types to match underlying class

--- packages/kickflip-cli/app/Application.php
+++ packages/kickflip-cli/app/Application.php
@@ -12,15 +12,19 @@ class Application extends BaseApplication
 {
     public static ?string $localBase;
 
+    // phpcs:disable
     /**
      * Get the path to the resources directory.
      *
+     * @param string $path
+     *
      * @return string
      */
-    public function resourcePath(string $path = '')
+    public function resourcePath($path = '')
     {
         $basePath = static::$localBase ?? $this->basePath;
 
         return $basePath . DIRECTORY_SEPARATOR . 'resources' . ($path ? DIRECTORY_SEPARATOR . $path : $path);
     }
+    // phpcs:enable
 }
