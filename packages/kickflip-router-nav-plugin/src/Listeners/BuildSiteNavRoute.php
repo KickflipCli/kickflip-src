1f83d18889b935c36fbc8b428735a2ef66644d5a
Dan Pock <dpock@liquidweb.com>
2022-01-05 16:03:05 -0500

    fix(plugins-router-nav): correct subdirectory based website routes

--- packages/kickflip-router-nav-plugin/src/Listeners/BuildSiteNavRoute.php
+++ packages/kickflip-router-nav-plugin/src/Listeners/BuildSiteNavRoute.php
@@ -7,6 +7,11 @@ namespace Kickflip\RouterNavPlugin\Listeners;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Str;
 use Kickflip\Events\PageDataCreated;
+use Kickflip\KickflipHelper;
+
+use function parse_url;
+
+use const PHP_URL_PATH;
 
 /**
  * An event handler that will listen for PageDataCreated events.
@@ -14,18 +19,26 @@ use Kickflip\Events\PageDataCreated;
  * This listener will determine the appropriate route name from a PageData's URL,
  * then registers a faux GET route with laravel's router.
  */
-class BuildSiteNavRoutes
+class BuildSiteNavRoute
 {
     public function handle(PageDataCreated $event)
     {
+        $baseDirectory = parse_url(KickflipHelper::config('site.baseUrl'), PHP_URL_PATH);
         $pageData = $event->pageData;
         $url = $pageData->getUrl();
-        // Register thin route for URL generating...
+
+        // Ensure index route has index name...
         if ($url === '/') {
             $routeName = 'index';
         } else {
             $routeName = (string) Str::of($url)->trim('/')->replace('/', '.');
         }
-        Route::name($routeName)->get($pageData->getUrl(), static fn () => '');
+
+        // Register a thin route based on the file name...
+        if ($baseDirectory !== null) {
+            Route::name($routeName)->prefix($baseDirectory)->get($pageData->getUrl(), static fn () => '');
+        } else {
+            Route::name($routeName)->get($pageData->getUrl(), static fn () => '');
+        }
     }
 }
