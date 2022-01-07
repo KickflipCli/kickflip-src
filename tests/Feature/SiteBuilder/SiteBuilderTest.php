efb64ceb46162790ba890e3150d70f2dcdf50c0a
Dan Pock <dpock@liquidweb.com>
2022-01-06 19:54:27 -0500

    refactor SiteBuilder tests

--- tests/Feature/SiteBuilder/SiteBuilderTest.php
+++ tests/Feature/SiteBuilder/SiteBuilderTest.php
@@ -4,66 +4,7 @@ declare(strict_types=1);
 
 namespace KickflipMonoTests\Feature\SiteBuilder;
 
-use Kickflip\Models\PageData;
-use Kickflip\SiteBuilder\ShikiNpmFetcher;
-use Kickflip\SiteBuilder\SiteBuilder;
-use Kickflip\SiteBuilder\SourcesLocator;
-use KickflipMonoTests\DataProviderHelpers;
-use KickflipMonoTests\TestCase;
-
-use function app;
-use function view;
-
-class SiteBuilderTest extends TestCase
+class SiteBuilderTest extends AbstractSiteBuilderTest
 {
-    use DataProviderHelpers;
-
-    protected const ENV = 'local';
-
-    public function setUp(): void
-    {
-        parent::setUp();
-        SiteBuilder::includeEnvironmentConfig(static::ENV);
-        SiteBuilder::updateBuildPaths(static::ENV);
-        SiteBuilder::updateAppUrl();
-        $shikiNpmFetcher = app(ShikiNpmFetcher::class);
-        if (!$shikiNpmFetcher->isShikiDownloaded()) {
-            $shikiNpmFetcher->installShiki();
-        }
-    }
-
-    public function tearDown(): void
-    {
-        $shikiNpmFetcher = app(ShikiNpmFetcher::class);
-        if ($shikiNpmFetcher->isShikiDownloaded()) {
-            $shikiNpmFetcher->removeShikiAndNodeModules();
-        }
-        parent::tearDown();
-    }
-
-    /**
-     * @dataProvider renderListDataProvider
-     */
-    public function testItWillProduceExpectedHtml(PageData $page): void
-    {
-        $view = view($page->source->getName(), [
-            'page' => $page,
-        ]);
-
-        self::assertMatchesHtmlSnapshot(self::stripMixIdsFromHtml($view->render()));
-    }
-
-    /**
-     * @return array<string, array{PageData}>
-     */
-    public function renderListDataProvider(): array
-    {
-        $this->refreshApplication();
-        /**
-         * @var SourcesLocator $sourceLocator
-         */
-        $sourceLocator = app(SourcesLocator::class);
-
-        return self::autoAddDataProviderKeys($sourceLocator->getRenderPageList());
-    }
+    protected static ?string $buildEnv = 'local';
 }
---
+++ tests/Feature/SiteBuilder/SubDirSiteBuilderTest.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+namespace KickflipMonoTests\Feature\SiteBuilder;
+
+class SubDirSiteBuilderTest extends AbstractSiteBuilderTest
+{
+    protected static ?string $buildEnv = 'production';
+}
