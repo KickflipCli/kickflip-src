814ae08952d06f06b52ffa4e06fb23e5c711a9f6
Dan Pock <dpock@liquidweb.com>
2022-02-04 19:44:43 -0500

    fix tests

--- tests/Unit/Models/SourcePageMetaDataTest.php
+++ tests/Unit/Models/SourcePageMetaDataTest.php
@@ -44,7 +44,6 @@ class SourcePageMetaDataTest extends TestCase
         self::assertIsString($sourcePageMetaData->getName());
         self::assertIsString($sourcePageMetaData->getFilename());
         self::assertIsString($sourcePageMetaData->getFullPath());
-        self::assertIsString($sourcePageMetaData->getRelativePath());
         self::assertIsString($sourcePageMetaData->getExtension());
         self::assertIsString($sourcePageMetaData->getMimeExtension());
         self::assertIsString($sourcePageMetaData->getType());
