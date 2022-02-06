61217321e4833878b3ad9931f22262a86313863c
Dan Pock <dpock@liquidweb.com>
2022-02-06 12:01:10 -0500

    fix(tests): move test check to feature where it should live

--- tests/Feature/SiteBuilder/BasicSourcesLocatorTest.php
+++ tests/Feature/SiteBuilder/BasicSourcesLocatorTest.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace KickflipMonoTests\Feature\SiteBuilder;
 
 use Kickflip\Models\PageData;
+use Kickflip\SiteBuilder\SiteBuilder;
 use Kickflip\SiteBuilder\SourcesLocator;
 use KickflipMonoTests\Feature\BaseFeatureTestCase;
 use KickflipMonoTests\ReflectionHelpers;
@@ -14,6 +15,14 @@ use function dirname;
 class BasicSourcesLocatorTest extends BaseFeatureTestCase
 {
     use ReflectionHelpers;
+
+    public function testItCanInstantiateSiteBuilder()
+    {
+        self::assertClassExists(SiteBuilder::class);
+        $siteBuilder = new SiteBuilder();
+        self::assertInstanceOf(SiteBuilder::class, $siteBuilder);
+        self::assertHasProperty($siteBuilder, 'sourcesLocator');
+    }
 
     public function testCanVerifySourcesLocatorProperties()
     {
