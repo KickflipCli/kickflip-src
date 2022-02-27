a4f96652c689bf930e4987c76a96a61ac28f42db
Dan <619938+mallardduck@users.noreply.github.com>
2022-02-26 21:25:13 -0500

    correct tests

--- tests/Feature/BaseFeatureTestCase.php
+++ tests/Feature/BaseFeatureTestCase.php
@@ -32,9 +32,6 @@ abstract class BaseFeatureTestCase extends BaseTestCase
     public bool $shouldRunShikiFetcher = true;
     public string $manifestPath = '/source/assets/build/mix-manifest.json';
 
-    // phpcs:ignore
-    public array $expectedOutputSubstrings = []; // Remove this once this is merged https://github.com/laravel-zero/foundation/pull/76
-
     public function basePath(): string
     {
         return realpath(__DIR__ . self::agnosticPath('/../../packages/kickflip'));
