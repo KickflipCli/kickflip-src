5af97f9c17bd9f6b011cfc99c586560000901135
Dan <619938+mallardduck@users.noreply.github.com>
2022-04-11 12:01:16 -0400

    adjust timing logger tests

--- tests/Feature/LoggerBaseFeatureTest.php
+++ tests/Feature/LoggerBaseFeatureTest.php
@@ -5,33 +5,61 @@ declare(strict_types=1);
 namespace KickflipMonoTests\Feature;
 
 use Illuminate\Config\Repository;
+use Illuminate\Support\Collection;
 use Kickflip\Logger;
 
+use ReflectionClass;
 use function app;
 
 class LoggerBaseFeatureTest extends BaseFeatureTestCase
 {
+
+    /**
+     * @var array{0: \ReflectionClass<Repository>, 1: \ReflectionProperty}
+     */
+    private array $timingReflection;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        Collection::macro('filterTimings', [static::class, 'stripTimeValues']);
+    }
+
+    public static function stripTimeValues($input)
+    {
+        if (is_float($input)) {
+            return '<DUMMY VALUE>';
+        }
+
+        if (is_array($input)) {
+            $subItems = collect($input);
+            return $subItems->map([static::class, 'stripTimeValues'])->toArray();
+        }
+
+        return $input;
+    }
+
     public function testLoggerTimingWorksCorrectly()
     {
         $timingsRepo = app('kickflipTimings');
         self::assertInstanceOf(Repository::class, $timingsRepo);
-        self::assertCount(4, $timingsRepo->all());
+        self::assertMatchesObjectSnapshot(Collection::filterTimings($timingsRepo->all()));
 
         // Make Step 1
         Logger::timing('NotStatic::stepOne');
-        self::assertCount(5, $timingsRepo->all());
+        self::assertMatchesObjectSnapshot(Collection::filterTimings($timingsRepo->all()));
         self::assertIsArray($timingsRepo->get('NotStatic'));
         self::assertCount(1, $timingsRepo->get('NotStatic'));
 
         // Make step 2
         Logger::timing('NotStatic::stepTwo', 'Static');
-        self::assertCount(5, $timingsRepo->all());
+        self::assertMatchesObjectSnapshot(Collection::filterTimings($timingsRepo->all()));
         self::assertIsArray($timingsRepo->get('NotStatic'));
         self::assertCount(2, $timingsRepo->get('NotStatic'));
 
         // Make step 3
         Logger::timing('Static::stepThree');
-        self::assertCount(6, $timingsRepo->all());
+        self::assertMatchesObjectSnapshot(Collection::filterTimings($timingsRepo->all()));
         self::assertIsArray($timingsRepo->get('Static'));
         self::assertCount(1, $timingsRepo->get('Static'));
     }
